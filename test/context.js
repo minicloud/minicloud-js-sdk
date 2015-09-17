2941c18571fe57ee5e5ce80c01326a1965039010
jim <jim@miniyun.cn>
2015-09-17 10:41:36 +0800

    members/get_my_account

--- test/context.js
+++ test/context.js
@@ -1,11 +1,29 @@
 global.timeout = 30000
+var uuid = require('uuid')
     /**
-     * Return init socket 
+     * Return init test case 
      * @api public
      */
 exports.init = function*() {
     if (!global.socket) {
-        var client = require('../lib') 
-        yield client.connect('http://demo.minicloud.io')
+        var Client = require('../lib')
+            //set current access device is web app
+        var clientId = 'JsQCsjF3yr7KACyT'
+        var clientSecret = 'bqGeM4Yrjs3tncJZ'
+        yield Client.connect('http://demo.minicloud.io', clientId, clientSecret)
+            //create member
+        var Member = new Client.Member()
+        var name = uuid.v4()
+        var data = yield Member.register(name, '123456')
+        if (data.status === 200) {
+            //member login
+            var Oauth2 = new Client.Oauth2()
+            data = yield Oauth2.token(name, '123456', 'js-sdk')
+            if (data.status === 200) {
+                //set access token
+                Client.setAccessToken(data.access_token)
+                global.userName = name
+            }
+        }
     }
 }
--- test/members/member.js
+++ test/members/member.js
@@ -3,21 +3,26 @@ var assert = require('assert')
 var uuid = require('uuid')
 describe('member.js', function() {
     this.timeout(global.timeout)
-    var memberClient = null
+    var Member = null
     before(function*(done) {
         yield context.init()
-        var ClientSDK = require('../../lib')
-        var MemberClient = ClientSDK.Member
-        memberClient = new MemberClient()
+        var Client = require('../../lib')
+        Member = new Client.Member()
         return done()
     })
     it('members/register', function*(done) {
         var name = uuid.v4()
-        var data = yield memberClient.register(name, '123456')
+        var data = yield Member.register(name, '123456')
         data.status.should.equal(200)
-        data = yield memberClient.register(name, '123456')
+        data = yield Member.register(name, '123456')
         data.status.should.equal(409)
         data.error.should.equal('member_existed')
+        done()
+    })
+    it('members/get_my_account', function*(done) {
+        var data = yield Member.getMyAccount()
+        data.status.should.equal(200)
+        data.name.should.equal(global.userName)
         done()
     })
 })
