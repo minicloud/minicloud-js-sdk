1ef07124889a2c6133cc92c40004e6611e5d1f73
jim <jim@miniyun.cn>
2015-09-24 13:00:14 +0800

    files/hash_upload & file simple upload

---
+++ test/files/file-upload.js
@@ -0,0 +1,27 @@
+var context = require('../context')
+var assert = require('assert')
+describe('client-simple-file-upload.js', function() {
+    this.timeout(global.timeout)
+    before(function*(done) {
+        yield context.init() 
+        return done()
+    })
+    it('file/upload simple file upload', function*(done) {
+        var Client = require('../../lib')
+        var localPath = './test/test-files/lt-1k.js'
+        var remotePath = '/home/test-files/lt-1k.js'
+        var uploader = new Client.ClientSimpleFileUpload(localPath, remotePath)
+        var data = yield uploader.run()
+        data.name.should.equal('lt-1k.js')
+        done()
+    })
+    it('file/upload hash upload', function*(done) {
+        var Client = require('../../lib')
+        var localPath = './test/test-files/lt-1k.js'
+        var remotePath = '/home/test-files/lt-测试.js'
+        var uploader = new Client.ClientSimpleFileUpload(localPath, remotePath)
+        var data = yield uploader.run()
+        data.name.should.equal('lt-测试.js')
+        done()
+    })
+})
